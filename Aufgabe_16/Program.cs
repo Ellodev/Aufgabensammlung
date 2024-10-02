b8666c3549659555e6ed9dc2774a66b4bc2afbc4
Ellodev <ello.developers@gmail.com>
2024-10-02 15:50:39 +0200

    feat: more things

---
+++ Aufgabe_16/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Aufgabe_16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your sentence:");
+        char[] vocals = ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü'];
+        int[] vocalCounter = new int[vocals.Length];
+        char[] input = Console.ReadLine().ToLower().ToCharArray();
+        int numberOfVocals = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == vocals[0] || input[i] == vocals[1] || input[i] == vocals[2] || input[i] == vocals[3] ||
+                input[i] == vocals[4] || input[i] == vocals[5] || input[i] == vocals[6] || input[i] == vocals[7])
+            {
+                numberOfVocals++;
+                int vocalLocation = Array.FindIndex(vocals, v => v == input[i]);
+                vocalCounter[vocalLocation]++;
+            }
+
+        }
+        
+        Console.WriteLine($"Vokale: {numberOfVocals}");
+        for (int i = 0; i < vocalCounter.Length; i++)
+        {
+            if (vocalCounter[i] != 0)
+            {
+                Console.WriteLine($"Der Vokal {vocals[i]} kommt {vocalCounter[i]} mal vor.");
+            }
+        }
+
+    }
+
+}
---
+++ Aufgabe_17/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Aufgabe_17
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                FetchJoke();
+                Console.WriteLine("Press Enter to get another joke...");
+                Console.ReadLine(); // Wait for user input before fetching the next joke
+                
+            }
+        }
+
+        static async void FetchJoke()
+        {
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync("https://witzapi.de/api/joke/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                
+                parsejoke(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
+        static void parsejoke(string responseBody)
+        {
+            Console.Write(JObject.Parse(responseBody)["data"].ToString());
+        }
+    }
+}
